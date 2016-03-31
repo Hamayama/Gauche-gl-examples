d92a5661cf429bee09228964664b2a7e3d2c6f33
Hamayama <hamay1010@gmail.com>
2016-03-31 12:41:33 +0900

    alaudplay.scm chg v1.02

--- alaudplay.scm
+++ alaudplay.scm
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; alaudplay.scm
-;; 2016-3-31 v1.01
+;; 2016-3-31 v1.02
 ;;
 ;; ＜内容＞
 ;;   Gauche-al を使って音楽を演奏するためのモジュールです。
@@ -31,6 +31,29 @@
     ))
 (select-module alaudplay)
 
+;; Gauche-al モジュールの定数のダミー設定
+(select-module al)
+(export
+  AL_PITCH AL_GAIN AL_MAX_DISTANCE AL_ROLLOFF_FACTOR AL_REFERENCE_DISTANCE
+  AL_MIN_GAIN AL_MAX_GAIN
+  AL_CONE_OUTER_GAIN AL_CONE_INNER_ANGLE AL_CONE_OUTER_ANGLE
+  AL_POSITION AL_VELOCITY AL_DIRECTION AL_SOURCE_RELATIVE
+  AL_LOOPING AL_BUFFER
+  AL_SOURCE_STATE AL_INITIAL AL_PLAYING AL_PAUSED AL_STOPPED
+  AL_BUFFERS_QUEUED AL_BUFFERS_PROCESSED)
+(define-values
+  (AL_PITCH AL_GAIN AL_MAX_DISTANCE AL_ROLLOFF_FACTOR AL_REFERENCE_DISTANCE
+   AL_MIN_GAIN AL_MAX_GAIN
+   AL_CONE_OUTER_GAIN AL_CONE_INNER_ANGLE AL_CONE_OUTER_ANGLE
+   AL_POSITION AL_VELOCITY AL_DIRECTION AL_SOURCE_RELATIVE
+   AL_LOOPING AL_BUFFER)
+  (apply values (make-list 16 0)))
+(define-values
+  (AL_SOURCE_STATE AL_INITIAL AL_PLAYING AL_PAUSED AL_STOPPED
+   AL_BUFFERS_QUEUED AL_BUFFERS_PROCESSED)
+  (apply values (make-list 7 0)))
+(select-module alaudplay)
+
 ;; Gauche-al モジュールのロード
 (define *al-loaded*
   (load "al" :error-if-not-found #f))
@@ -38,9 +61,10 @@
 ;; 音楽演奏機能の有効/無効状態
 (define *aud-enabled* #t)
 
-;; 音楽演奏機能が有効か
+;; 音楽演奏機能が有効かどうか
 (define (aud-enabled?)
   (and *aud-enabled* *al-loaded*))
+
 
 ;; 音楽演奏機能の初期化
 (define (aud-init :optional (enabled #t))
