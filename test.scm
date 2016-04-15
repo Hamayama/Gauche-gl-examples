3651feaee1ca7fc098ef53e4bc5653914802d1e2
Hamayama <hamay1010@gmail.com>
2016-04-15 11:51:03 +0900

    test.scm chg

--- test.scm
+++ test.scm
@@ -5,9 +5,6 @@
 (add-load-path "." :relative)
 (display #\cr)(flush) ; allocate console for windows
 (use gauche.test)
-(use file.util)
-
-(define old-err-port (current-error-port (open-output-file (null-device))))
 
 (test-start "alaudplay")
 (use alaudplay)
@@ -24,7 +21,6 @@
 (test-module 'gltextscrn)
 (test-end)
 
-(current-error-port old-err-port)
 (print "HIT ENTER KEY!")
 (flush)
 (read-line)
