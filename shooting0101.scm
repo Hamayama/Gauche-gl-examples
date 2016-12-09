07f247443a075dda2aff4ce44e0e8fc21cf9a7cc
Hamayama <hamay1010@gmail.com>
2016-12-09 17:42:53 +0900

    shooting0101.scm chg v1.72

--- shooting0101.scm
+++ shooting0101.scm
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; shooting0101.scm
-;; 2016-12-7 v1.71
+;; 2016-12-9 v1.72
 ;;
 ;; ＜内容＞
 ;;   Gauche-gl を使用した、簡単なシューティングゲームです。
@@ -463,13 +463,13 @@
   ;; (敵ミサイル(上側))
   (set-char-drawer #\| (lambda (x y width height chw chh z)
                          (gl-color 0.7 0.7 0.7 1.0)
-                         (draw-win-rect (+ x (* chw 0.4)) (+ y (* chh 0.1))
-                                        (* chw 0.2) (* chh 0.9) width height 'left z)))
+                         (draw-win-rect (+ x (* chw 0.4)) y
+                                        (* chw 0.2) chh width height 'left z)))
   ;; (敵ミサイル(下側))
   (set-char-drawer #\V (lambda (x y width height chw chh z)
                          (gl-color 0.9 0.9 0.9 1.0)
                          (draw-win-poly x y (vector (f32vector 0 0)
-                                                    (f32vector (* chw 0.5) (* chh 0.9))
+                                                    (f32vector (* chw 0.5) chh)
                                                     (f32vector chw 0))
                                         width height z)))
   ;; 音楽データの初期化
