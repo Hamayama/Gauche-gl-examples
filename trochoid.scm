c3dd2b3dea5eef0353c4b9f962f7350d4e779e30
Hamayama <hamay1010@gmail.com>
2017-08-10 21:16:59 +0900

    trochoid.scm chg v1.20

--- trochoid.scm
+++ trochoid.scm
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; trochoid.scm
-;; 2017-8-8 v1.19
+;; 2017-8-10 v1.20
 ;;
 ;; ＜内容＞
 ;;   Gauche-gl を使って、内トロコイド曲線を描くサンプルです。
@@ -39,29 +39,28 @@
     #f32(1.0 0.0 0.0 1.0) #f32(1.0 0.0 1.0 1.0) #f32(1.0 1.0 0.0 1.0) #f32(1.0 1.0 1.0 1.0)))
 
 ;; 内トロコイド曲線の座標と色を設定
-(define (setup-trochoid rc rm rd color-index vvec cvec vec-offset)
+(define (setup-trochoid rc rm rd color vvec cvec vec-offset)
   (let* ((vnum 0)
          (t1   (-  rc rm))
          (t2   (/. t1 rm))
          (n    (/. rm (gcd t1 rm))) ; 何周すれば始点に戻るか(←t2を約分したときの分母に等しい)
-         (x1   0)
-         (y1   0)
-         (colr (~ *color-table* color-index 0))
-         (colg (~ *color-table* color-index 1))
-         (colb (~ *color-table* color-index 2)))
+         (colr (~ color 0))
+         (colg (~ color 1))
+         (colb (~ color 2)))
     ;; 座標と色を計算して、ユニフォームベクタに格納する
-    (do ((rad  0 (+ rad (/. 2pi 360)))
-         (i1   (* vec-offset 2) (+ i1 2))
-         (i2   (* vec-offset 3) (+ i2 3)))
-        ((>= rad (* 2pi n)) #f)
-      (set! x1 (+ (* t1 (cos rad)) (* rd     (cos (* t2 rad)))))
-      (set! y1 (+ (* t1 (sin rad)) (* (- rd) (sin (* t2 rad)))))
-      (f32vector-set! vvec i1       x1)   ; X座標
-      (f32vector-set! vvec (+ i1 1) y1)   ; Y座標
-      (f32vector-set! cvec i2       colr) ; R
-      (f32vector-set! cvec (+ i2 1) colg) ; G
-      (f32vector-set! cvec (+ i2 2) colb) ; B
-      (inc! vnum))
+    (do ((i1 0                (+ i1 1))
+         (i2 (* vec-offset 2) (+ i2 2))
+         (i3 (* vec-offset 3) (+ i3 3)))
+        ((>= i1 (* 360 n)) #f)
+      (let* ((rad (* pi/180 i1))
+             (x1  (+ (* t1 (cos rad)) (* rd     (cos (* t2 rad)))))
+             (y1  (+ (* t1 (sin rad)) (* (- rd) (sin (* t2 rad))))))
+        (f32vector-set! vvec i2       x1)   ; X座標
+        (f32vector-set! vvec (+ i2 1) y1)   ; Y座標
+        (f32vector-set! cvec i3       colr) ; R
+        (f32vector-set! cvec (+ i3 1) colg) ; G
+        (f32vector-set! cvec (+ i3 2) colb) ; B
+        (inc! vnum)))
     ;; 頂点数を返す
     vnum))
 
@@ -78,7 +77,9 @@
   (set! *vvec* (make-f32vector (* (+ (* 360 *rc*) 1) 2) 0))
   (set! *cvec* (make-f32vector (* (+ (* 360 *rc*) 1) 3) 0))
   ;; 内トロコイド曲線の座標と色を設定
-  (set! *vnum* (setup-trochoid *rc* *rm* *rd* *color-index* *vvec* *cvec* 0))
+  (set! *vnum* (setup-trochoid *rc* *rm* *rd*
+                               (~ *color-table* *color-index*)
+                               *vvec* *cvec* 0))
   ;; OpenGLの配列に設定する
   (gl-enable-client-state GL_VERTEX_ARRAY)
   (gl-enable-client-state GL_COLOR_ARRAY)
@@ -113,7 +114,9 @@
     (set! *rd*          (randint 10 *rm*))
     (set! *color-index* (randint 10 15))
     ;; 内トロコイド曲線の座標と色を設定
-    (set! *vnum* (setup-trochoid *rc* *rm* *rd* *color-index* *vvec* *cvec* 0))
+    (set! *vnum* (setup-trochoid *rc* *rm* *rd*
+                                 (~ *color-table* *color-index*)
+                                 *vvec* *cvec* 0))
     ;; タイトル文字列を更新
     (glut-set-window-title (format "~a (~d,~d)" *title* *rm* *rd*))
     ;; 画面表示
